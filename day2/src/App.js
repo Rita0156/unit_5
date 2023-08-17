3f4dde9551b0b02123cb402916682cecc90b2246
Rita0156 <ritachaudhari006@gmail.com>
2023-08-17 22:19:18 +0530

    day2 re

--- day2/src/App.js
+++ day2/src/App.js
@@ -1,9 +1,20 @@
 //import logo from './logo.svg';
 import './App.css';
+import TodoCard from './compo/TodoCard';
 
 function App() {
   return (
     <div className="App">
+       
+       
+       <div style={{width:"90%",margin:"auto",backgroundColor:"black"}}>
+            <h1>Title</h1>
+            <h1>Category</h1>
+            <h1>Price</h1>
+            <h1>Toggle</h1>
+            <h1>Delete</h1>
+        </div>
+        <TodoCard/>
       
     </div>
   );
